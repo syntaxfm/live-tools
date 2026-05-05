cf237a85acc87b1a5b036f06465ef22d1a44c023
Scott Tolinski <scott.tolinski@gmail.com>
2026-05-05 11:59:33 -0600

    removes cf config

--- svelte.config.js
+++ svelte.config.js
@@ -10,20 +10,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({
-			// See below for an explanation of these options
-			config: undefined,
-			platformProxy: {
-				configPath: undefined,
-				environment: undefined,
-				persist: undefined
-			},
-			fallback: 'plaintext',
-			routes: {
-				include: ['/*'],
-				exclude: ['<all>']
-			}
-		}),
+		adapter: adapter(),
 
 		typescript: {
 			config: (config) => ({
